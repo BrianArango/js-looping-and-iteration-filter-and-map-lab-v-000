4182a601b960df437c293e1af78fa416591d88cc
Brian Arango <brianvictus@gmail.com>
2019-08-01 20:15:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ function driversWithRevenueOver (drivers, revenue) {
 }
 
 function driverNamesWithRevenueOver (drivers, revenue) {
-  return driversWithRevenueOver(drivers, revenue)
-    .map(function (driver) {
+  return driversWithRevenueOver(drivers, revenue).map(function (driver) {
       return driver.name;
     });
 }
